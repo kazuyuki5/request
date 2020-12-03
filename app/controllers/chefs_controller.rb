35188c00941df3a63d812becf3032a944e50fe17
kazuyuki5 <kazuyuki2957@gmail.com>
2020-12-03 18:10:30 +0900

    ログイン画面の設定

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,11 +1,18 @@
 class ChefsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
-    @chefs = Chef.all
+    @chefs = Chef.new
   end
 
   def new
     @chef = Chef.new
   end
-  
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
